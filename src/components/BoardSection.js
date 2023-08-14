f6d7f5fe8a6d6706d1e8931cad6fee4b7aebfcf9
bahmanashtari <bahman778@gmail.com>
2023-08-14 13:24:39 -0700

    update: stylings

--- src/components/BoardSection.js
+++ src/components/BoardSection.js
@@ -3,12 +3,12 @@ import Task from './Task'
 const BoardSection = props => {
 	return (
 		<div>
-			<h1 className='text-orange-200'>Col Name</h1>
+			<h1 className='text-white'>Col Name</h1>
 			<Task />
 			<Task />
 			<Task />
-			<button type='button' className='text-green-500'>
-				Add
+			<button type='button' className='text-green-600'>
+				+
 			</button>
 		</div>
 	)
--- src/components/MainBoard.js
+++ src/components/MainBoard.js
@@ -4,7 +4,7 @@ const MainBoard = props => {
 	return (
 		<div className='bg-zinc-700 flex items-center justify-center h-screen'>
 			<div className='bg-slate-700 shadow-2xl rounded-lg p-8 w-5/6 h-4/5'>
-				<div className='h-10 align-text-top text-white font-bold text-2xl'>
+				<div className='h-10 align-text-top text-gray-400 font-bold text-2xl'>
 					Board Name
 				</div>
 				<div className='h-full grid grid-flow-col'>
