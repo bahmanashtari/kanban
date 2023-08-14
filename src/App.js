3ca286467581af9011ebf4135500ed7657571b14
bahmanashtari <bahman778@gmail.com>
2023-08-14 11:08:34 -0700

    add: main board as a component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import logo from './logo.svg'
 import './App.css'
+
+import MainBord from './components/MainBord'
 
 function App() {
 	return (
 		<div className='App'>
 			<header className='headercell'></header>
 			<body>
-				<div className='bg-zinc-700 flex items-center justify-center h-screen'>
-					<div className='bg-slate-800 shadow-2xl rounded-lg p-8 w-5/6 h-4/5'>
-						{/* Your Task components will go here */}
-					</div>
-				</div>
+				<MainBord />
 			</body>
 		</div>
 	)
---
+++ src/components/MainBoard.js
@@ -0,0 +1,9 @@
+const MainBoard = props => {
+	return (
+		<div className='bg-zinc-700 flex items-center justify-center h-screen'>
+			<div className='bg-slate-800 shadow-2xl rounded-lg p-8 w-5/6 h-4/5'>
+				{/* Your Task components will go here */}
+			</div>
+		</div>
+	)
+}
