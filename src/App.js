55409ef155156a22471085830093ebdc5e415ade
bahmanashtari <bahman778@gmail.com>
2023-08-14 10:37:11 -0700

    add: the basic main board

--- src/App.js
+++ src/App.js
@@ -4,19 +4,14 @@ import './App.css'
 function App() {
 	return (
 		<div className='App'>
-			<header className='headercell'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'>
-					Learn React
-				</a>
-			</header>
+			<header className='headercell'></header>
+			<body>
+				<div className='bg-zinc-700 flex items-center justify-center h-screen'>
+					<div className='bg-slate-800 shadow-2xl rounded-lg p-8 w-5/6 h-4/5'>
+						{/* Your Task components will go here */}
+					</div>
+				</div>
+			</body>
 		</div>
 	)
 }
