b457b4f2b511d251c7a85f03fbaf4145af301c9a
soroush behmardi <sori.bk@gmail.com>
2023-08-25 13:28:18 -0700

    some chanes

--- src/components/BoardSection.js
+++ src/components/BoardSection.js
@@ -1,78 +1,75 @@
-import { useState } from 'react'
+import { useState, useRef } from "react";
 
-import Task from './Task'
+import Task from "./Task";
 
-const BoardSection = props => {
-	const [taskIdCount, setTaskIdCount] = useState(1)
-	const [tasks, setTasks] = useState([])
+const BoardSection = (props) => {
+  const counter = useRef(0);
+  const [tasks, setTasks] = useState([]);
 
-	const addEmptyTask = () => {
-		const newEmptyTask = { id: taskIdCount, description: '', likeCount: 0 }
-		setTasks(prevTasks => [...prevTasks, newEmptyTask])
-		setTaskIdCount(prevId => prevId + 1)
-	}
+  const addEmptyTask = () => {
+    const newEmptyTask = { id: counter.current, description: "", likeCount: 0 };
+    setTasks((prevTasks) => [...prevTasks, newEmptyTask]);
+    counter.current++;
+  };
 
-	const descriptionHandler = (taskId, description) => {
-		const updatedTasks = tasks.map(task => {
-			if (task.id === taskId) {
-				return { ...task, description: description }
-			}
+  const descriptionHandler = (taskId, description) => {
+    const updatedTasks = tasks.map((task) => {
+      if (task.id === taskId) {
+        return { ...task, description: description };
+      }
 
-			return task
-		})
-		setTasks(updatedTasks)
-	}
+      return task;
+    });
+    setTasks(updatedTasks);
+  };
 
-	const likeHandler = taskId => {
-		const updatedTasks = tasks.map(task => {
-			if (task.id === taskId) {
-				return { ...task, likeCount: task.likeCount + 1 }
-			}
+  const likeHandler = (taskId) => {
+    const updatedTasks = tasks.map((task) => {
+      if (task.id === taskId) {
+        return { ...task, likeCount: task.likeCount + 1 };
+      }
 
-			return task
-		})
-		setTasks(updatedTasks)
-	}
+      return task;
+    });
+    setTasks(updatedTasks);
+  };
 
-	const deleteHandler = taskId => {
-		const updatedTasks = tasks.filter(task => task.id !== taskId)
-		setTasks(updatedTasks)
-	}
+  const deleteHandler = (taskId) => {
+    const updatedTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(updatedTasks);
+  };
 
-	const handleOnDragOver = event => {
-		event.preventDefault()
-	}
+  const handleOnDragOver = (event) => {
+    event.preventDefault();
+  };
 
-	const handleOnDrop = event => {
-		const id = event.dataTransfer.getData('id')
-		const draggedTask = tasks.find(task => task.id === id)
-		setTasks([...tasks, draggedTask])
-	}
+  const handleOnDrop = (event) => {
+    const id = event.dataTransfer.getData("id");
+    const draggedTask = tasks.find((task) => task.id === id);
+    setTasks([...tasks, draggedTask]);
+  };
 
-	const tasksToShow = tasks.map(task => (
-		<Task
-			taskKey={task.id}
-			taskContent={task}
-			onDescription={descriptionHandler}
-			onLike={likeHandler}
-			onDelete={deleteHandler}
-		/>
-	))
+  const tasksToShow = tasks.map((task) => (
+    <Task
+      taskKey={task.id}
+      taskContent={task}
+      onDescription={descriptionHandler}
+      onLike={likeHandler}
+      onDelete={deleteHandler}
+    />
+  ));
 
-	return (
-		<section>
-			<h1 className='text-white'>{props.name}</h1>
-			<ul onDragOver={handleOnDragOver} onDrop={handleOnDrop}>
-				{tasksToShow}
-			</ul>
-			<button
-				type='button'
-				className='text-green-600'
-				onClick={addEmptyTask}>
-				+
-			</button>
-		</section>
-	)
-}
+  return (
+    <section>
+      <h1 className="text-white">{props.name}</h1>
+      <ul onDragOver={handleOnDragOver} onDrop={handleOnDrop}>
+        {tasksToShow}
+      </ul>
+      <button type="button" className="text-green-600" onClick={addEmptyTask}>
+        +
+      </button>
+    </section>
+  );
+};
 
-export default BoardSection
+export default BoardSection;
--- src/components/MainBoard.js
+++ src/components/MainBoard.js
@@ -1,52 +1,69 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import BoardSection from './BoardSection'
-import Filters from './Filters'
-import CreateNewBoard from './CreateNewBoard'
+import BoardSection from "./BoardSection";
+import Filters from "./Filters";
+import CreateNewBoard from "./CreateNewBoard";
 
-const MainBoard = props => {
-	const [boardName, setBoardName] = useState('')
-	const [sections, setSections] = useState([])
+const globalState = {
+  baordname: "test",
+  searchKey: "a",
+  sections: [
+    {
+      key: "sectionID",
+      name: "secion1",
+      tasks: [
+        { key: "id1", description: "some text", like: 0, createDate: "" },
+        { key: "id2", description: "some text2", like: 2, createDate: "" },
+      ],
+    },
+  ],
+  viewSections: null, // sectionID
+  sortBy: null, // votes
+};
 
-	const createBoardHandler = boardDetails => {
-		const boardName = boardDetails.boardName
-		const sectionNames = Object.entries(boardDetails).reduce(
-			(acc, [key, val]) => {
-				if (key.startsWith('sectionName_')) {
-					acc = [...acc, val]
-				}
-				return acc
-			},
-			[]
-		)
-		setBoardName(boardName)
-		setSections(sectionNames)
-	}
+const MainBoard = (props) => {
+  const [boardName, setBoardName] = useState("");
+  const [sections, setSections] = useState([]);
 
-	const sectionsToShow = sections.map(section => (
-		<BoardSection name={section} />
-	))
+  const createBoardHandler = (boardDetails) => {
+    const boardName = boardDetails.boardName;
+    const sectionNames = Object.entries(boardDetails).reduce(
+      (acc, [key, val]) => {
+        if (key.startsWith("sectionName_")) {
+          acc = [...acc, val];
+        }
+        return acc;
+      },
+      []
+    );
+    setBoardName(boardName);
+    setSections(sectionNames);
+  };
 
-	return (
-		<div className='bg-zinc-700 flex items-center justify-center h-screen'>
-			<div className='bg-slate-700 shadow-2xl rounded-lg p-8 w-5/6 h-4/5'>
-				<section className='h-10 align-text-top text-gray-400 font-bold text-2xl'>
-					{boardName}
-				</section>
-				{sections.length > 0 && (
-					<section>
-						<Filters />
-					</section>
-				)}
-				<section className='grid grid-flow-col'>
-					{sections.length > 0 && sectionsToShow}
-					{sections.length === 0 && (
-						<CreateNewBoard onCreateBoard={createBoardHandler} />
-					)}
-				</section>
-			</div>
-		</div>
-	)
-}
+  const sectionsToShow = sections.map((section) => (
+    <BoardSection name={section} />
+  ));
 
-export default MainBoard
+  return (
+    <div className="bg-zinc-700 flex items-center justify-center h-screen">
+      <div className="bg-slate-700 shadow-2xl rounded-lg p-8 w-5/6 h-4/5">
+        <section className="h-10 align-text-top text-gray-400 font-bold text-2xl">
+          {boardName}
+        </section>
+        {sections.length > 0 && (
+          <section>
+            <Filters />
+          </section>
+        )}
+        <section className="grid grid-flow-col">
+          {sections.length > 0 && sectionsToShow}
+          {sections.length === 0 && (
+            <CreateNewBoard onCreateBoard={createBoardHandler} />
+          )}
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default MainBoard;
