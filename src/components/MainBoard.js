4aaaf4492f85aaa376992f241ecea0923cbd2c0c
bahmanashtari <bahman778@gmail.com>
2023-08-25 15:23:22 -0700

    update: + add global state to MainBoard

--- src/components/MainBoard.js
+++ src/components/MainBoard.js
@@ -1,69 +1,73 @@
-import { useState } from "react";
+import { useState } from 'react'
 
-import BoardSection from "./BoardSection";
-import Filters from "./Filters";
-import CreateNewBoard from "./CreateNewBoard";
+import BoardSection from './BoardSection'
+import Filters from './Filters'
+import CreateNewBoard from './CreateNewBoard'
 
-const globalState = {
-  baordname: "test",
-  searchKey: "a",
-  sections: [
-    {
-      key: "sectionID",
-      name: "secion1",
-      tasks: [
-        { key: "id1", description: "some text", like: 0, createDate: "" },
-        { key: "id2", description: "some text2", like: 2, createDate: "" },
-      ],
-    },
-  ],
-  viewSections: null, // sectionID
-  sortBy: null, // votes
-};
+const initialState = {
+	baordname: '',
+	searchKey: '',
+	viewSections: null, // sectionID
+	sortBy: null, // votes
+	sections: [
+		// {
+		//   sectionKey: "sectionID",
+		//   sectionName: "secionName",
+		//   tasks: [
+		//     { taskKey: "id1", description: "some text", like: 0, createDate: "" },
+		//     { ... }, ...
+		//   ],
+		// },
+		// { ... }, { ... }, ...
+	],
+}
 
-const MainBoard = (props) => {
-  const [boardName, setBoardName] = useState("");
-  const [sections, setSections] = useState([]);
+const MainBoard = () => {
+	const [globalState, setGlobalState] = useState(initialState)
 
-  const createBoardHandler = (boardDetails) => {
-    const boardName = boardDetails.boardName;
-    const sectionNames = Object.entries(boardDetails).reduce(
-      (acc, [key, val]) => {
-        if (key.startsWith("sectionName_")) {
-          acc = [...acc, val];
-        }
-        return acc;
-      },
-      []
-    );
-    setBoardName(boardName);
-    setSections(sectionNames);
-  };
+	const createBoardHandler = boardDetails => {
+		const boardName = boardDetails.boardName
+		setGlobalState(currentState => {
+			return {
+				...currentState,
+				boardName,
+				sections: Object.entries(boardDetails).reduce((acc, [key, val]) => {
+					if (key.startsWith('sectionName_')) {
+						acc = [...acc, { sectionKey: key, sectionName: val, task: [] }]
+					}
+					return acc
+				}, []),
+			}
+		})
+	}
 
-  const sectionsToShow = sections.map((section) => (
-    <BoardSection name={section} />
-  ));
+	return (
+		<div className='bg-zinc-700 flex items-center justify-center h-screen'>
+			<div className='bg-slate-700 shadow-2xl rounded-lg p-8 w-5/6 h-4/5'>
+				<section className='h-10 align-text-top text-gray-400 font-bold text-2xl'>
+					{globalState.baordname}
+				</section>
+				{globalState.sections.length > 0 && (
+					<section>
+						<Filters />
+					</section>
+				)}
+				<section className='grid grid-flow-col'>
+					{globalState.sections.length === 0 && (
+						<CreateNewBoard onCreateBoard={createBoardHandler} />
+					)}
+					{globalState.sections.length > 0 &&
+						globalState.sections.map(section => (
+							<BoardSection
+								sectionName={section.sectionName}
+								secitonKey={section.sectionKey}
+								sectionTasks={section.sectionTasks}
+							/>
+						))}
+				</section>
+			</div>
+		</div>
+	)
+}
 
-  return (
-    <div className="bg-zinc-700 flex items-center justify-center h-screen">
-      <div className="bg-slate-700 shadow-2xl rounded-lg p-8 w-5/6 h-4/5">
-        <section className="h-10 align-text-top text-gray-400 font-bold text-2xl">
-          {boardName}
-        </section>
-        {sections.length > 0 && (
-          <section>
-            <Filters />
-          </section>
-        )}
-        <section className="grid grid-flow-col">
-          {sections.length > 0 && sectionsToShow}
-          {sections.length === 0 && (
-            <CreateNewBoard onCreateBoard={createBoardHandler} />
-          )}
-        </section>
-      </div>
-    </div>
-  );
-};
-
-export default MainBoard;
+export default MainBoard
